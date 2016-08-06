2097358d8a6d46c5e3ef677eea1d4c638373fb57
raz818 <csmaster701@gmail.com>
2016-08-06 15:30:34 -0500

    Player is moving, platforms are generating, and the camera is following.

---
+++ HappyRunner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+        lastPlayerPosition = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceToMove = player.transform.position.x - lastPlayerPosition.x;
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPosition = player.transform.position;
+	}
+}
---
+++ HappyRunner/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject platform;
+    public Transform generationPoint;
+    public float distinceBetween;
+
+    private float platformWidth;
+
+	// Use this for initialization
+	void Awake () {
+        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GeneratePlatform();
+	}
+
+    public void GeneratePlatform()
+    {
+        if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distinceBetween, transform.position.y, 0);
+
+            Instantiate(platform, generationPoint.transform.position, generationPoint.transform.rotation);
+        }
+    }
+}
